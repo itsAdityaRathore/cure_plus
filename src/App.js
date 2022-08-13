59e1fb0f10d7c41aca9777ae8014503c9f913976
itsDikshaMahanot <jainmani786@gmail.com>
2022-08-13 14:53:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button } from '@mui/material';
 function App() {
   return (
     <>
-      <Button variant='contained'>Hello World</Button>
+      <Button>Hello World</Button>
     </>
   );
 }
