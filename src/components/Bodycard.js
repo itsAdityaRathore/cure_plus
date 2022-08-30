a0978f0ea13f7d4a584c976a052035dc587174ee
itsDikshaMahanot <jainmani786@gmail.com>
2022-08-30 20:34:02 +0530

    bodycard chnges done

--- src/components/Bodycard.js
+++ src/components/Bodycard.js
@@ -1,22 +1,16 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import { Card } from '@mui/material'
+import { Box, Container } from '@mui/system'
+import React from 'react'
 import '../../src/App.css'
 
 function Bodycard() {
     return (
-        <Card className='card'  sx={{ minWidth: 275 , marginX:0.2, boxShadow:20}}>
-            <CardContent >
-                heloo world
-            </CardContent>
-        </Card>
+        <Container maxWidth='false' sx={{ background: '#20b255', height:'80vh' }}>
+            <Box maxWidth='false' className='img' sx={{ backgroundImage: 'url(https://images.unsplash.com/photo-1661553914969-1c781d515fae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80)' }}>
+                
+            </Box>
+        </Container>
     )
 }
 
 export default Bodycard
-
-
-
-
-
-
