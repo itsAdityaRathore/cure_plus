01dae8ac72a618deac9d3564721cd50577c23ce7
itsDikshaMahanot <jainmani786@gmail.com>
2022-08-30 20:57:10 +0530

    background added

--- src/components/Bodycard.js
+++ src/components/Bodycard.js
@@ -3,10 +3,11 @@ import { Box, Container } from '@mui/system'
 import React from 'react'
 import '../../src/App.css'
 
+
 function Bodycard() {
     return (
         <Container maxWidth='false' sx={{ background: '#20b255', height:'80vh' }}>
-            <Box maxWidth='false' className='img' sx={{ backgroundImage: 'url(https://images.unsplash.com/photo-1661553914969-1c781d515fae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80)' }}>
+            <Box maxWidth='false' className='img' sx={{ backgroundImage: 'url( https://www.vithoulkas.com/sites/default/files/news/homeopathy-11.jpg)' }}>
                 
             </Box>
         </Container>
