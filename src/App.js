393fc041d151545a68459b7cee71b0dd62551f8c
Aditya Rathore <aditya.rathore@binarytouch.com>
2022-09-04 19:35:54 +0530

    Banner Image Fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 import * as React from 'react';
 import Navbar from './components/Navbar';
-import Bodycard from './components/Bodycard';
 import { StyledEngineProvider } from '@mui/material';
+import BannerImage from './components/banner_image/BannerImage';
 import ContactUs from './components/contact_us/ContactUs';
 
 function App() {
   return (
-
     <StyledEngineProvider injectFirst>
       <Navbar />
-      <Bodycard />
+      <BannerImage />
       <ContactUs/>
     </StyledEngineProvider>
-
   );
 }
 
--- src/components/banner_image/BannerImage.js
+++ src/components/banner_image/BannerImage.js
@@ -1,17 +1,16 @@
-import { Card } from '@mui/material'
 import { Box, Container } from '@mui/system'
 import React from 'react'
-import '../../src/App.css'
+import './BannerImage.css'
 
 
-function Bodycard() {
+function BannerImage() {
     return (
-        <Container maxWidth='false' sx={{ background: '#20b255', height:'80vh' }}>
+        <Container maxWidth='false' sx={{ background: '#20b255', height: '80vh' }}>
             <Box maxWidth='false' className='img' sx={{ backgroundImage: 'url( https://www.vithoulkas.com/sites/default/files/news/homeopathy-11.jpg)' }}>
-                
+
             </Box>
         </Container>
     )
 }
 
-export default Bodycard
+export default BannerImage
