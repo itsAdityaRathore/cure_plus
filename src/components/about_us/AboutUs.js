9b4621dc89f189337a9e9efcc1bdba281c99e79b
itsDikshaMahanot <jainmani786@gmail.com>
2022-09-19 14:12:01 +0530

    text in center added

--- src/components/about_us/AboutUs.js
+++ src/components/about_us/AboutUs.js
@@ -7,12 +7,12 @@ function AboutUs() {
     return (
         <Container className='about-us-box' sx={{ marginY: { md: "-33rem", xs: "-60rem" },color: { md: 'white', xs: 'white' } }}>
             <Stack direction={{ md: "row" }}>
-                <Box className='about-us-content' display='flex'
+                <Box className='about-us-content' 
                     sx={{}}>
                     <img className='dr-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRytX65eKyfPMKlTXg_xHv60IK5IGabz8omLg&usqp=CAU'></img>
                 </Box>
                 <Box className='about-us-content'
-                    sx={{ width:'100%',  }}>
+                    sx={{backgroundColor:'lightslategray', width:'100%',  }}>
                     <h1>
                         Cure Plus Homeopathic Clinic - Go Green Inside
                     </h1>
