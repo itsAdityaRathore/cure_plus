66a83f22fc5052c0852fffde7427000d5a388455
itsDikshaMahanot <jainmani786@gmail.com>
2022-09-19 14:06:39 +0530

    image center resolved

--- src/components/about_us/AboutUs.js
+++ src/components/about_us/AboutUs.js
@@ -5,14 +5,14 @@ import "./AboutUs.css"
 
 function AboutUs() {
     return (
-        <Container className='about-us-box' sx={{ marginY: { md: "-33rem", xs: "-55rem" }, color: { md: 'white', xs: 'white' } }}>
+        <Container className='about-us-box' sx={{ marginY: { md: "-33rem", xs: "-60rem" },color: { md: 'white', xs: 'white' } }}>
             <Stack direction={{ md: "row" }}>
-                <Box className='about-us-content'
-                    sx={{marginY:'1rem',}}>
-                    <img width='fit-content' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRytX65eKyfPMKlTXg_xHv60IK5IGabz8omLg&usqp=CAU'></img>
+                <Box className='about-us-content' display='flex'
+                    sx={{}}>
+                    <img className='dr-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRytX65eKyfPMKlTXg_xHv60IK5IGabz8omLg&usqp=CAU'></img>
                 </Box>
                 <Box className='about-us-content'
-                    sx={{alignItems:{md:'center'}, }}>
+                    sx={{ width:'100%',  }}>
                     <h1>
                         Cure Plus Homeopathic Clinic - Go Green Inside
                     </h1>
