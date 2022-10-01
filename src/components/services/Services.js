6971edd7195340b257b73cba147786700789b964
itsDikshaMahanot <jainmani786@gmail.com>
2022-10-01 22:49:56 +0530

    paper carditem craeted

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,39 +1,52 @@
-import { Box } from '@mui/material'
-import { Container, Stack } from '@mui/system'
+import styled from '@emotion/styled'
+import { Container, Grid, Paper } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
-import "./Services.css"
+
+const Paperitem = styled(Paper)
+    `
+color: #000;
+:hover {
+  color: #4caf50;
+}
+text-align: center;
+height:40vh;
+margin-top:0.5rem;
+margin-bottom:0.5rem;
+padding-top:1rem;
+padding-bottom:1rem;
+`;
 
 function Services() {
     return (
-        <Box className='service-box'>
-            <Container className='service-box-container' sx={{}}>
-                <Stack
-                    direction={{ md: 'row', xs: 'column' }}
-                >
-                    <Box className='service-box-card' sx={{ backgroundColor: 'green', }}>
-                        Different services cards
-                        {/* height for box above image */}
-                        {/* <Box  sx={{ backgroundColor: 'gray', height:'50%', width:'50%' }}>
-                            <img className='service-card-icon' src='https://e7.pngegg.com/pngimages/653/624/png-clipart-computer-icons-mind-thought-logo-prototype-miscellaneous-psychology.png'></img>  
-                        </Box>
-                        <Box>
-                            hello
-                        </Box> */}
-                    </Box>
-                    <Box className='service-box-card' sx={{ backgroundColor: 'green' }}>
-                        Different services cards
-                    </Box>
-                    <Box className='service-box-card' sx={{ backgroundColor: 'green' }}>
-                        Different services cards
-                    </Box>
-                    <Box className='service-box-card' sx={{ backgroundColor: 'green' }}>
-                        Different services cards
-                    </Box>
-                </Stack>
-            </Container>
+        <Box sx={{ backgroundColor: '#599c2a', margin: '5%', paddingBottom: '1rem', paddingLeft: '1rem', paddingRight: '1rem', height:'100%' }}>
+            <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+                <Grid item xs={10} sm={6} md={3}>
+                    <Paperitem>
+                        xs=8
+                    </Paperitem>
+                </Grid>
+                <Grid item xs={10} sm={6} md={3}>
+                    <Paperitem>
+                        xs=4
+                    </Paperitem>
+                </Grid>
+                <Grid item xs={10} sm={6} md={3}>
+                    <Paperitem>
+                        xs=4
+                    </Paperitem>
+                </Grid>
+                <Grid item xs={10} sm={6} md={3}>
+                    <Paperitem>
+                        xs=8
+                    </Paperitem>
+                </Grid>
+            </Grid>
 
-        </Box >
+        </Box>
     )
 }
 
 export default Services
+
+
