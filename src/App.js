67edc77bcd6b4fcbe59d53aa7c72bfebfe58ed6a
itsDikshaMahanot <jainmani786@gmail.com>
2022-10-01 20:35:12 +0530

    1. services card created and added 2. need to add icon still and other details

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navbar from "./components/navbar/Navbar";
 import { StyledEngineProvider } from "@mui/material";
 import BannerImage from "./components/banner_image/BannerImage";
 import ContactUs from "./components/contact_us/ContactUs";
+import Services from "./components/services/Services";
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
       <Navbar />
       <BannerImage />
-      <ContactUs />    
+      <ContactUs />
+      <Services />       
     </StyledEngineProvider>
   );
 }
