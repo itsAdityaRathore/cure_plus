ddbabb49a286dcc9c0c8c11ecca7326ed2d56b69
Aditya Rathore <adityarathore35@gmail.com>
2022-10-03 23:38:43 +0530

    services heading added

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,52 +1,53 @@
-import styled from '@emotion/styled'
-import { Container, Grid, Paper } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
+import { Container, Grid, Paper, styled } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
-const Paperitem = styled(Paper)
-    `
-color: #000;
-:hover {
-  color: #4caf50;
-}
-text-align: center;
-height:40vh;
-margin-top:0.5rem;
-margin-bottom:0.5rem;
-padding-top:1rem;
-padding-bottom:1rem;
+const Paperitem = styled(Paper)`
+  :hover {
+    box-shadow: 1px 1px 15px #0000006c;
+  }
+  border-radius:12px;
+  text-align: center;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  height:50vh;
 `;
+Paperitem.defaultProps={elevation:8};
 
 function Services() {
-    return (
-        <Box sx={{ backgroundColor: '#599c2a', margin: '5%', paddingBottom: '1rem', paddingLeft: '1rem', paddingRight: '1rem', height:'100%' }}>
-            <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-                <Grid item xs={10} sm={6} md={3}>
-                    <Paperitem>
-                        xs=8
-                    </Paperitem>
-                </Grid>
-                <Grid item xs={10} sm={6} md={3}>
-                    <Paperitem>
-                        xs=4
-                    </Paperitem>
-                </Grid>
-                <Grid item xs={10} sm={6} md={3}>
-                    <Paperitem>
-                        xs=4
-                    </Paperitem>
-                </Grid>
-                <Grid item xs={10} sm={6} md={3}>
-                    <Paperitem>
-                        xs=8
-                    </Paperitem>
-                </Grid>
-            </Grid>
-
-        </Box>
-    )
+  return (
+    <>
+    <Container className="text-center">
+           <p className="mt-20 text-xl text-slate-500 font-medium "> Services </p>
+           <h5 className="font-semibold text-3xl font-sans">Services we provide</h5>
+    </Container>
+    <Box
+      sx={{
+        marginTop: "5%",
+        marginBottom: "5%",
+        paddingBottom: "3%",
+        paddingX:'10%',
+      }}
+    >
+      <Grid container spacing={5} sx={{ justifyContent: "center" }}>
+        <Grid item xs={10} sm={6} md={3}>
+          <Paperitem >xs=8</Paperitem>
+        </Grid>
+        <Grid item xs={10} sm={6} md={3}>
+          <Paperitem>xs=4</Paperitem>
+        </Grid>
+        <Grid item xs={10} sm={6} md={3}>
+          <Paperitem>xs=4</Paperitem>
+        </Grid>
+        <Grid item xs={10} sm={6} md={3}>
+          <Paperitem>xs=8</Paperitem>
+        </Grid>
+      </Grid>
+    </Box>
+    </>
+  );
 }
 
-export default Services
-
-
+export default Services;
