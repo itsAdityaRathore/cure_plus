f2f79c3d17cf8c484d2683f16482c331f4f79767
itsDikshaMahanot <jainmani786@gmail.com>
2022-10-05 23:02:56 +0530

    Chnges done to layout for Small screen i.e column-2

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -18,8 +18,8 @@ Paperitem.defaultProps = { elevation: 8 };
 function Services() {
   return (
     <>
-      <Container className="bg-blue-500 my-10 pb-4 lg:grid-cols-3 md:grid grid-cols-2 gap-3 sm:columns-2">
-        <Box className="mt-16 min-w-200 ">
+      <Container className="my-10 p-4 lg:grid-cols-3 md:grid grid-cols-2 gap-3 sm:columns-2">
+        <Box className="mt-16 min-w-200">
           <Paper
             elevation="5"
             className="py-4 px-5 text-center rounded-xl m-5 bg-gradient-to-b from-teal-50"
@@ -219,7 +219,7 @@ function Services() {
             <h2 className="pt-5 text-violet-600 font-medium">+ READMORE</h2>
           </Paper>
         </Box>
-        <Box className="mt-16 min-w-200 ">
+        {/* <Box className="mt-16 min-w-200 ">
           <Paper
             elevation="5"
             className="py-4 px-5 text-center rounded-xl m-5 bg-gradient-to-b from-teal-50"
@@ -244,6 +244,31 @@ function Services() {
             <h2 className="pt-5 text-violet-600 font-medium">+ READMORE</h2>
           </Paper>
         </Box>
+        <Box className="mt-16 min-w-200 ">
+          <Paper
+            elevation="5"
+            className="py-4 px-5 text-center rounded-xl m-5 bg-gradient-to-b from-teal-50"
+          >
+            <Box
+              width={100}
+              height={100}
+              className="relative mx-auto  bg-gradient-to-b
+             from-yellow-200 rounded-full -mt-12 overflow-hidden shadow-md shadow-slate-400"
+            >
+              <img
+                alt="img"
+                className="object-cover fill"
+                src="https://picsum.photos/100"
+              />
+            </Box>
+            <h4 className="text-xl py-4">ENT</h4>
+            <p className="max-w-md mx-auto">
+              Objectively integrate enterprise-wide strategic theme areas with
+              functionalized infrastructures.
+            </p>
+            <h2 className="pt-5 text-violet-600 font-medium">+ READMORE</h2>
+          </Paper>
+        </Box> */}
       </Container>
     </>
   );
