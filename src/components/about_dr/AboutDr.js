c54970c7316485f9f6d2cb47d09f1d58e1f8fc7f
Aditya Rathore <adityarathore35@gmail.com>
2022-10-26 14:53:15 +0530

    font fix about dr

--- src/components/about_dr/AboutDr.js
+++ src/components/about_dr/AboutDr.js
@@ -22,34 +22,34 @@ function AboutDr() {
                 className="h-20 w-15 mb-0 "
                 src="https://t3.ftcdn.net/jpg/01/34/24/46/240_F_134244635_QdTMgtjMfLQealOg6WQSEc0UaRrxSrhj.jpg"
               />
-              <Typography className="lg:text-4xl text-left text-2xl my-auto font-bold  ">
+              <Typography className="lg:text-3xl text-left text-2xl my-auto font-bold  font-sans">
                 About US
               </Typography>
             </Box>
 
-            <h3 className="lg:text-3xl text-left text-xl font-medium ">
+            <h3 className="lg:text-2xl text-left text-xl font-medium font-sans font-semibold">
               Welcome To Doctorate Central Hospital
             </h3>
-            <p className="text-lg py-5 leading-8 text-justify">
+            <p className="text-lg py-5  leading-8 text-justify font-sans">
               Our team of highly trained professionals uses the latest healing
               technologies to restore you to pain-free health quickly and
               easily.
             </p>
             <Paper className="bg-blue-900 rounded-md p-2 justify-center h-100 my-8">
               <Container className="grid lg:grid-cols-2 gap-2 p-2 my-5 h-5/6">
-                <Box className=" p-2 text-left text-2xl font-semibold font-sans text-white flex">
+                <Box className="p-2 text-left text-xl font-semibold font-sans text-white flex">
                   <CheckCircleOutlineOutlinedIcon className="mr-2 mt-1" />
                   20+ years of excellence
                 </Box>
-                <Box className="p-2 text-left  text-2xl font-semibold font-sans text-white flex">
+                <Box className="p-2 text-left  text-xl font-semibold font-sans text-white flex">
                   <CheckCircleOutlineOutlinedIcon className="mr-2 mt-1" />
                   Multi Speciality
                 </Box>
-                <Box className=" p-2 text-left text-2xl font-semibold font-sans text-white flex">
+                <Box className="p-2 text-left text-xl font-semibold font-sans text-white flex">
                   <CheckCircleOutlineOutlinedIcon className="mr-2 mt-1" />
                   24 Hours Medical Service
                 </Box>
-                <Box className="p-2 text-left text-2xl font-semibold font-sans text-white flex ">
+                <Box className="p-2 text-left text-xl font-semibold font-sans text-white flex ">
                   <CheckCircleOutlineOutlinedIcon className="mr-2 mt-1" />
                   Professional Experts
                 </Box>
--- src/components/contact_us/ContactUs.js
+++ src/components/contact_us/ContactUs.js
@@ -5,7 +5,6 @@ import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import React from "react";
 import "./ContactUs.css";
 import { green } from "@mui/material/colors";
-import AboutUs from "../about_us/AboutUs";
 
 const IconBoxRound = styled(Box)({
   margin: 10,
@@ -22,99 +21,97 @@ const IconBoxRound = styled(Box)({
 
 function ContactUs() {
   return (
-    <>   
-    
-    <Container
-      maxWidth="md"
-      className="contact-us-box"
-      sx={{
-        width: { xs: "90vw", sm: "90vw", md: "80vw" },
-        marginTop: { md: "-4rem", xs: "-.5rem" },
-      }}
-    >
-      <Stack direction={{ md: "row" }}>
-        <Box
-          flex={1}
-          className="contact-us-box-item"
-          sx={{ backgroundColor: "white" }}
-        >
-          <IconBoxRound>
-            <PhoneInTalkOutlinedIcon />
-          </IconBoxRound>
-          <Box className="contact-us-box-text">
-            <Typography
-              marginBottom="3px"
-              variant="subtitle2"
-              fontFamily={"inherit"}
-              color="#9e9e9e"
-            >
-              Phone No.
-            </Typography>
-            <Typography
-              variant="inherit"
-              fontFamily={"inherit"}
-              color="#353535"
-            >
-              +91-7676808586
-            </Typography>
-          </Box>
-        </Box>
-        <Box
-          flex={1}
-          className="contact-us-box-item"
-          sx={{ backgroundColor: "#599c2a", color: "#fff" }}
-        >
-          <IconBoxRound sx={{ color: "#fff", borderColor: "#ffffff55" }}>
-            <AccessAlarmRoundedIcon />
-          </IconBoxRound>
-          <Box className="contact-us-box-text">
-            <Typography
-              marginBottom="3px"
-              variant="subtitle2"
-              fontFamily={"inherit"}
-            >
-              Open Hours
-            </Typography>
-            <Stack>
-              <Typography variant="inherit" fontFamily={"inherit"}>
-                11:00 AM - 12:30 PM
+    <>
+      <Container
+        maxWidth="md"
+        className="contact-us-box"
+        sx={{
+          width: { xs: "90vw", sm: "90vw", md: "80vw" },
+          marginTop: { md: "-4rem", xs: "-.5rem" },
+        }}
+      >
+        <Stack direction={{ md: "row" }}>
+          <Box
+            flex={1}
+            className="contact-us-box-item"
+            sx={{ backgroundColor: "white" }}
+          >
+            <IconBoxRound>
+              <PhoneInTalkOutlinedIcon />
+            </IconBoxRound>
+            <Box className="contact-us-box-text">
+              <Typography
+                marginBottom="3px"
+                variant="subtitle2"
+                fontFamily={"inherit"}
+                color="#9e9e9e"
+              >
+                Phone No.
               </Typography>
-              <Typography v variant="inherit" fontFamily={"inherit"}>
-                5:30 AM - 8:00 PM
+              <Typography
+                variant="inherit"
+                fontFamily={"inherit"}
+                color="#353535"
+              >
+                +91-7676808586
               </Typography>
-            </Stack>
+            </Box>
           </Box>
-        </Box>
-        <Box
-          flex={1}
-          className="contact-us-box-item"
-          sx={{ backgroundColor: "white" }}
-        >
-          <IconBoxRound>
-            <LocationOnOutlinedIcon />
-          </IconBoxRound>
-          <Box className="contact-us-box-text">
-            <Typography
-              marginBottom="3px"
-              variant="subtitle2"
-              fontFamily={"inherit"}
-              color="#9e9e9e"
-            >
-              Our Address
-            </Typography>
-            <Typography
-              variant="inherit"
-              fontFamily={"inherit"}
-              color="#353535"
-            >
-              Shop E, BLOCK-C, Nester Raga, Dr. Puneetha Rajkumar Rd, B,
-              Chinappa Layout, Mahadevpura, Bengaluru, Karnatka - 560048
-            </Typography>
+          <Box
+            flex={1}
+            className="contact-us-box-item"
+            sx={{ backgroundColor: "#599c2a", color: "#fff" }}
+          >
+            <IconBoxRound sx={{ color: "#fff", borderColor: "#ffffff55" }}>
+              <AccessAlarmRoundedIcon />
+            </IconBoxRound>
+            <Box className="contact-us-box-text">
+              <Typography
+                marginBottom="3px"
+                variant="subtitle2"
+                fontFamily={"inherit"}
+              >
+                Open Hours
+              </Typography>
+              <Stack>
+                <Typography variant="inherit" fontFamily={"inherit"}>
+                  11:00 AM - 12:30 PM
+                </Typography>
+                <Typography v variant="inherit" fontFamily={"inherit"}>
+                  5:30 AM - 8:00 PM
+                </Typography>
+              </Stack>
+            </Box>
           </Box>
-        </Box>
-      </Stack>
-      
-    </Container>
+          <Box
+            flex={1}
+            className="contact-us-box-item"
+            sx={{ backgroundColor: "white" }}
+          >
+            <IconBoxRound>
+              <LocationOnOutlinedIcon />
+            </IconBoxRound>
+            <Box className="contact-us-box-text">
+              <Typography
+                marginBottom="3px"
+                variant="subtitle2"
+                fontFamily={"inherit"}
+                color="#9e9e9e"
+              >
+                Our Address
+              </Typography>
+              <Typography
+                variant="inherit"
+                fontFamily={"inherit"}
+                color="#353535"
+              >
+                Shop E, BLOCK-C, Nester Raga, Dr. Puneetha Rajkumar Rd, B,
+                Chinappa Layout, Mahadevpura, Bengaluru, Karnatka - 560048
+              </Typography>
+            </Box>
+          </Box>
+        </Stack>
+      </Container>
     </>
   );
 }
