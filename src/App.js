6117b45d65fd4e3a39927d56cbae923bcd1773cb
Aditya Rathore <adityarathore35@gmail.com>
2022-11-03 17:49:07 +0530

    FAQ seciton accordion

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ import ContactUs from "./components/contact_us/ContactUs";
 import Footer from "./components/footer/Footer";
 import CureFor from "./components/cure_for/CureFor";
 import AboutDr from "./components/about_dr/AboutDr";
+import Accordion from "./components/faq/Accordion";
+
+const questionsAnswers = [
+  {
+    question: "How many team members can I invite?",
+    answer:
+      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+  },
+  {
+    question: "What is the maximum file upload size?",
+    answer:
+      "No more than 2GB. All files in your account must fit your allotted storage space.",
+  },
+  {
+    question: "How do I reset my password?",
+    answer: `Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.`,
+  },
+  {
+    question: "Can I cancel my subscription?",
+    answer: `Yes! Send us a message and we’ll process your request no questions asked.`,
+  },
+  {
+    question: "Do you provide additional support?",
+    answer: `Chat and email support is available 24/7. Phone lines are open during normal business hours.Chat and email support is available 24/7. Phone lines are open during normal business hours.Chat and email support is available 24/7. Phone lines are open during normal business hours.`,
+  },
+];
 
 function App() {
   return (
@@ -16,6 +42,7 @@ function App() {
       <ContactUs />
       <AboutDr />
       <CureFor />
+      <Accordion questionsAnswers={questionsAnswers} />
       <Footer />
     </StyledEngineProvider>
   );
